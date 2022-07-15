529976a172b00f8ed449cbddaa10ee0a314e998d
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-15 11:01:27 +0200

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Counter extends Component {
     
     state={
-        count :1,
-        tags: ["tag1","tag2","tag3"]
+        value : this.props.value
+        
     };
 
     styles ={
@@ -15,31 +15,29 @@ class Counter extends Component {
         fontWeight :'bold'
     };
 
-    renderTags(){
-        if (this.state.tags===0) return <p>there are no tags!</p>
-        else
-        return <span>{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}</span>;
-    }
         
-    
+    handleIncrement = () =>
+    {
+        this.setState( {value : this.state.value + 1} );
+    }
 
     render() { 
-
-        
-
+    
         return (
         <div>
-            {this.renderTags()}
+            
+            <span className={this.getBadgeClasses()}>{this.state.value}</span>
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
             
         </div>);
     }
 
-    /* getBadgeClasses() {
+    getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
-
+/*
     formatCount(){
         const {count}=this.state;
         return count===0 ? "Zero" : count;
