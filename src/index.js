9c54384eace1c4398f2d09c2161100684328c8d9
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-07-15 11:01:11 +0200

    use of multiple components in react

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Counter from "./components/counter.jsx"
+import Counters from "./components/counters.jsx"
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<Counter/>, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
